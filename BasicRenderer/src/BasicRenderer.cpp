e43ce48a6a8899868bd490b5d03db1c69470d725
Panthuncia <matthew.h.gomes@gmail.com>
2025-09-17 02:50:28 -0400

    default scene

--- BasicRenderer/src/BasicRenderer.cpp
+++ BasicRenderer/src/BasicRenderer.cpp
@@ -260,20 +260,20 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
     auto baseScene = std::make_shared<Scene>();
 
-    //auto dragonScene = LoadModel("models/dragon.glb");
-    //dragonScene->GetRoot().set<Components::Scale>({ 5, 5, 5 });
-    //dragonScene->GetRoot().set<Components::Position>({ 0.0, 1, 0.0 });
+    auto dragonScene = LoadModel("models/dragon.glb");
+    dragonScene->GetRoot().set<Components::Scale>({ 5, 5, 5 });
+    dragonScene->GetRoot().set<Components::Position>({ 0.0, 1, 0.0 });
 
-    //auto carScene = LoadModel("models/porche.glb");
-    //carScene->GetRoot().set<Components::Scale>({ 0.6, 0.6, 0.6 });
-    //carScene->GetRoot().set<Components::Position>({ 1.0, 0.0, 1.0 });
+    auto carScene = LoadModel("models/porche.glb");
+    carScene->GetRoot().set<Components::Scale>({ 0.6, 0.6, 0.6 });
+    carScene->GetRoot().set<Components::Position>({ 1.0, 0.0, 1.0 });
 
-    //auto mountainScene = LoadModel("models/terrain.glb");
-    //mountainScene->GetRoot().set<Components::Scale>({ 50.0, 50.0, 50.0 });
-    //mountainScene->GetRoot().set<Components::Position>({ 0.0, -2.0, 0.0 });
+    auto mountainScene = LoadModel("models/terrain.glb");
+    mountainScene->GetRoot().set<Components::Scale>({ 50.0, 50.0, 50.0 });
+    mountainScene->GetRoot().set<Components::Position>({ 0.0, -2.0, 0.0 });
 
-    //auto tigerScene = LoadModel("models/tiger.glb");
-    //tigerScene->GetRoot().set<Components::Scale>({ 0.01, 0.01, 0.01 });
+    auto tigerScene = LoadModel("models/tiger.glb");
+    tigerScene->GetRoot().set<Components::Scale>({ 0.01, 0.01, 0.01 });
 
     //auto usdScene = LoadModel("models/sponza.usdz");
     
@@ -291,22 +291,24 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     //renderer.GetCurrentScene()->AppendScene(usdScene->Clone());
     //renderer.GetCurrentScene()->AppendScene(curtains->Clone());
 
-    //renderer.GetCurrentScene()->AppendScene(dragonScene->Clone());
-    //renderer.GetCurrentScene()->AppendScene(carScene->Clone());
-    //renderer.GetCurrentScene()->AppendScene(mountainScene->Clone());
-    //renderer.GetCurrentScene()->AppendScene(tigerScene->Clone());
+    renderer.GetCurrentScene()->AppendScene(dragonScene->Clone());
+    renderer.GetCurrentScene()->AppendScene(carScene->Clone());
+    renderer.GetCurrentScene()->AppendScene(mountainScene->Clone());
+    renderer.GetCurrentScene()->AppendScene(tigerScene->Clone());
 
     //renderer.GetCurrentScene()->AppendScene(bistro->Clone());
     //renderer.GetCurrentScene()->AppendScene(wine->Clone());
     
-	//renderer.GetCurrentScene()->AppendScene(robot->Clone());
-    renderer.GetCurrentScene()->AppendScene(sphereScene->Clone());
+	renderer.GetCurrentScene()->AppendScene(robot->Clone());
+
+    //renderer.GetCurrentScene()->AppendScene(sphereScene->Clone());
 
     //for (int i = 0; i < 1; i++) {
     //    auto sphereInstance = renderer.GetCurrentScene()->AppendScene(sphereScene->Clone());
     //    auto point = getRandomPointInVolume(-20, 20, -2, 2, -20, 20);
     //    sphereInstance->GetRoot().set<Components::Position>({ point.x, point.y, point.z });
     //}
+
 
     renderer.SetEnvironment("sky");
 
